f8f06b84cabdc9cdb3b9b8a2f2b40cdb46e37d8f
Room107 <harit.pink@dome.tu.ac.th>
2024-11-06 18:36:29 +0700

    Initial commit

---
+++ src/main/java/com/example/crud/StudentController.java
@@ -0,0 +1,28 @@
+/*package com.example.crud;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class StudentController {
+
+	@Autowired
+	private StudentRepository userRepository;
+	
+	@GetMapping
+	public List<Students> getAlluser(){
+		return userRepository.findAll();
+	}
+	
+	@PostMapping
+	public Students createStudents(@RequestBody Students Students ) {
+		return userRepository.save(Students);
+	}
+}*/
